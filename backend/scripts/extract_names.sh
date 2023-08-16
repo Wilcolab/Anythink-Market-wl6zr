8ab19703ab3b4a393aeac07b3e485d9645954906
Anirudh Dayanand <94374523+AnirudhDaya@users.noreply.github.com>
2023-08-16 17:27:42 +0000

    updated extract_names.sh

--- backend/scripts/extract_names.sh
+++ backend/scripts/extract_names.sh
@@ -1,16 +1,2 @@
-#!/bin/bash
-
-read -p "Enter file name: " file_name
-output_file="amazon_names.txt"
-
-if [ -f "$file_name" ]; then
-    awk -F ',' 'BEGIN { OFS = "," } {
-        if (tolower($4) ~ /@amazon\.com/) {
-            print $3 " " $2
-        }
-    }' "$file_name" > "$output_file"
-
-    echo "Names have been written to $output_file"
-else
-    echo "File not found."
-fi
+#!/usr/bin/bash
+grep -i "@amazon.com" $1 | awk -F, '{print $3, $2}' | tr "," " " >  output_names.txt
