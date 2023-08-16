2e28b4434e7adcfe2f181c7865c10894bd10c7ae
Anirudh Dayanand <94374523+AnirudhDaya@users.noreply.github.com>
2023-08-16 17:09:41 +0000

    updated extract_names.sh

--- backend/scripts/extract_names.sh
+++ backend/scripts/extract_names.sh
@@ -4,16 +4,11 @@ read -p "Enter file name: " file_name
 output_file="amazon_names.txt"
 
 if [ -f "$file_name" ]; then
-    ans=()
-    while IFS=',' read -r first_name last_name email; do
-        if [[ "$email" == *"@amazon.com"* ]]; then
-            ans+=("$first_name $last_name")
-        fi
-    done < "$file_name"
-
-    for name in "${ans[@]}"; do
-        echo "$name" >> "$output_file"
-    done
+    awk -F ',' 'BEGIN { OFS = "," } {
+        if (tolower($4) ~ /@amazon\.com/) {
+            print $3 " " $2
+        }
+    }' "$file_name" > "$output_file"
 
     echo "Names have been written to $output_file"
 else
